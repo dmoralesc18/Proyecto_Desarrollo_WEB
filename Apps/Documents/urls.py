72fd98b81c76dd5f4ca0630e7cc758e86a5d4504
dmoralesc18 <dmoralesc18@miumg.edu.gt>
2025-10-02 10:07:22 -0600

    Modulo de documentos

--- Apps/Documents/urls.py
+++ Apps/Documents/urls.py
@@ -1,8 +1,18 @@
-from django.contrib import admin
-from django.urls import path, include
-from Apps.Documents import views
-from .views import DocumentsView
+from django.urls import path
+from .views import (
+    DocumentsView,
+    DocumentCreateView,
+    DocumentDetailView,
+    DocumentUpdateView,
+    DocumentDeleteView
+)
+
+app_name = 'documents'
 
 urlpatterns = [
-   path('documents/', DocumentsView.as_view(), name='documentsapp'),
+    path('', DocumentsView.as_view(), name='documentsapp'),
+    path('create/', DocumentCreateView.as_view(), name='document_create'),
+    path('<int:pk>/', DocumentDetailView.as_view(), name='document_detail'),
+    path('<int:pk>/edit/', DocumentUpdateView.as_view(), name='document_update'),
+    path('<int:pk>/delete/', DocumentDeleteView.as_view(), name='document_delete'),
 ]
