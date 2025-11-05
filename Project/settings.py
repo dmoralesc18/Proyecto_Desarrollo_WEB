67f467aac4dfd9bf9f12df408f8066cb4162aaaf
dmoralesc18 <dmoralesc18@miumg.edu.gt>
2025-11-04 23:36:40 -0600

    tercera prueba de carga de pagina de login

--- Project/settings.py
+++ Project/settings.py
@@ -161,7 +161,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'Apps/Home/static'),
+    os.path.join(BASE_DIR, '/Apps/Home/static/'),
 ]
 
 if not DEBUG:
