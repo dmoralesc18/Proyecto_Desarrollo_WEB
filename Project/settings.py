9ea83c8626c5e6c2fff34126ff715eb37a5920bd
dmoralesc18 <dmoralesc18@miumg.edu.gt>
2025-11-04 23:19:15 -0600

    Carrga de la pagina

--- Project/settings.py
+++ Project/settings.py
@@ -160,6 +160,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Home/static'),
+]
+
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
